9ec227096a544ba88e41a3d995e8a5de6d6edb21
ThunderRabbit <zuzazibubabi@gmail.com>
2025-04-02 12:05:38 +0800

    added some styles and imports for specific tailwind handling

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="header flex justify-between w-full fixed">
+    <div className="header flex justify-between w-full fixed items-center py-4 px-8">
       <div className="headerIcon">Color Haven</div>
-      <div className="headerNav flex gap-4">
+      <div className="headerNav flex gap-4 items-center">
         <div className="aboutNav">About</div>
         <div className="toolsNav">Tools</div>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,15 @@ export default function Home() {
   return (
     <div className="page">
       <Header />
-      <div className="MainPage"></div>
+      <div className="MainPage pt-13">
+        <div className="rectangleDiv">
+          <div className="r1"></div>
+          <div className="r2"></div>
+          <div className="r3"></div>
+          <div className="r4"></div>
+          <div className="r5"></div>
+        </div>
+      </div>
     </div>
   );
 }
