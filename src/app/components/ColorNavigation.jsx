b1c95247c86fbc01f3c743ee7e6e6c5f1e06ceb9
ThunderRabbit <zuzazibubabi@gmail.com>
2025-04-06 23:37:42 +0800

    working on the color picker

--- src/app/components/ColorNavigation.jsx
+++ src/app/components/ColorNavigation.jsx
@@ -2,6 +2,13 @@ export default function ColorNavigation() {
   return (
     <div className="colorNav">
       <div className="rectange-tester r1 bg-black w-64 h-64 rounded-2xl"></div>
+      <div className="colorSelector">
+        <input
+          type="color"
+          //   value={color}
+          //   onChange={(e) => setColor(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
