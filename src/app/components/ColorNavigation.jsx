88b6d36c44d010aaeb3ff529997cfa6d195b17c4
ThunderRabbit <zuzazibubabi@gmail.com>
2025-04-06 23:07:17 +0800

    added a test block

---
+++ src/app/components/ColorNavigation.jsx
@@ -0,0 +1,7 @@
+export default function ColorNavigation() {
+  return (
+    <div className="colorNav">
+      <div className="rectange-tester r1 bg-black w-64 h-64 rounded-2xl"></div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,22 @@
+import BlobIllustration from "./components/BlobIllustration";
+import ColorNavigation from "./components/ColorNavigation";
 import Header from "./components/Header";
+import Selection from "./components/Selection";
 
 export default function Home() {
   return (
     <div className="page">
       <Header />
       <div className="MainPage pt-13">
-        <div className="rectangleDiv">
-          <div className="r1"></div>
-          <div className="r2"></div>
-          <div className="r3"></div>
-          <div className="r4"></div>
-          <div className="r5"></div>
+        <div className="p1">
+          <BlobIllustration />
+          <ColorNavigation />
+          <Selection />
         </div>
+        <div className="rectangleDiv"></div>
       </div>
     </div>
   );
 }
+
+//
