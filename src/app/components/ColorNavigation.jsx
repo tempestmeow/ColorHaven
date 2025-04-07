3a9cfdf27626fad80480e739de2ce0e3f4db4b91
ThunderRabbit <zuzazibubabi@gmail.com>
2025-04-07 18:28:37 +0800

    edited the color swatch

--- src/app/components/ColorNavigation.jsx
+++ src/app/components/ColorNavigation.jsx
@@ -1,12 +1,20 @@
+"use client";
+import { useState } from "react";
+
 export default function ColorNavigation() {
+  const [color, setColor] = useState("#000000");
   return (
     <div className="colorNav">
-      <div className="rectange-tester r1 bg-black w-64 h-64 rounded-2xl"></div>
-      <div className="colorSelector">
+      <div
+        className="rectange-tester r1 w-64 h-64 rounded-2xl"
+        style={{ backgroundColor: color }}
+      ></div>
+      <div className="colorSelector ">
         <input
           type="color"
-          //   value={color}
-          //   onChange={(e) => setColor(e.target.value)}
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          className="selectColor cursor-pointer colorSwatch"
         />
       </div>
     </div>
