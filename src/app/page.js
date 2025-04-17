e5d5248665390d66079ad9e795db538dbfd83349
ThunderRabbit <zuzazibubabi@gmail.com>
2025-04-17 22:30:24 +0800

    added an illustration

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,19 @@
 import BlobIllustration from "./components/BlobIllustration";
 import ColorNavigation from "./components/ColorNavigation";
+import ColorSelectorIllustration from "./components/ColorSelectorIllustration";
 import Header from "./components/Header";
 import Selection from "./components/Selection";
 
 export default function Home() {
   return (
-    <div className="page">
+    <div className="page flex-col items-center justify-center">
       <Header />
-      <div className="MainPage pt-13">
-        <div className="p1">
-          <BlobIllustration />
+      <div className="MainPage pt-17 flex justify-center items-center">
+        <div className="p1 flex justify-center items-center">
+          <ColorSelectorIllustration />
         </div>
       </div>
       <ColorNavigation />
     </div>
   );
 }
-
-//
