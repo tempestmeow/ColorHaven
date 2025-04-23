c8cbad960af84bc00646833a0e6aeff4e9932b2b
ThunderRabbit <zuzazibubabi@gmail.com>
2025-04-23 22:36:11 +0800

    made the backend work using an api

--- src/app/components/ContactForm.jsx
+++ src/app/components/ContactForm.jsx
@@ -16,6 +16,32 @@ export default function ContactForm() {
     }));
   };
 
+  const WEB3FORMS_ACCESS_KEY = process.env.NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          access_key: WEB3FORMS_ACCESS_KEY,
+          ...formData,
+          subject: `New contact form submission from ${formData.name}`,
+        }),
+      });
+
+      const result = await response.json();
+      if (result.success) {
+      } else {
+        throw new Error(result.message || "Submit failed");
+      }
+    } catch (error) {}
+  };
+
   useEffect(() => {
     console.log(formData);
   }, [formData]);
@@ -23,15 +49,22 @@ export default function ContactForm() {
   return (
     <>
       <div className="form-container pt-[3rem]">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="name-input border-black border-4">
-            <input type="text" id="name" name="name" onChange={handleChange} />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
           </div>
           <div className="email-input border-black border-4">
             <input
               type="text"
               id="email"
               name="email"
+              value={formData.email}
               onChange={handleChange}
             />
           </div>
@@ -40,6 +73,7 @@ export default function ContactForm() {
               type="text"
               id="feedback"
               name="feedback"
+              value={formData.feedback}
               onChange={handleChange}
             />
           </div>
